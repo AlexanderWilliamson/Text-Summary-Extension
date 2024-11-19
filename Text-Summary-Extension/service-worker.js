13f00610cc739d5afb387429dd9a57e9cb128e5f
Alexander Williamson <160697078+AlexanderWilliamson@users.noreply.github.com>
2024-11-18 19:28:11 -0500

    Add files via upload
    Added the main feature of being able to summarize text, and added a display box for the results. The current shortcut to trigger this is ctrl+alt+z. Note that to test this I was simply putting my apikey in as a variable, this has been removed in this version because of privacy so to run this version a key must be entered manually into the code.

--- Text-Summary-Extension/service-worker.js
+++ Text-Summary-Extension/service-worker.js
@@ -1,3 +1,49 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	if (message.type !== "text-getter-selected-text")
+		return;
+	
 	console.log(message);
+	const apiKey;
+	/*chrome.storage.sync.get("text-summarizer-api-key", function(res){
+		if (res)
+			apiKey = res;
+	});*/
+	
+	if (!apiKey){
+		sendResponse({"type": "service-worker-summary-result", "data": "Could not get apikey, click on the extension and enter apikey to run this extension"});
+		return;
+	}
+	
+	const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
+	
+	fetch(url, {
+		method: 'POST',
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			"contents": [
+				{
+					"parts": [
+						{
+							"text": `Summarize the following text: \"${message.data}\"`
+						}
+					]
+				}
+			]
+		})
+	}).then(response => {
+		console.log("We got to the http error check");
+		if(!response.ok) {
+			throw new Error(`HTTP error: ${response.status}`);
+		}
+		return response.json();
+	}).then(data => {
+		console.log("success, we are outputting the result");
+		sendResponse({"type": "service-worker-summary-result", "data": data.candidates[0].content.parts[0].text});
+	}).catch(error => {
+		console.log("womp womp error happened");
+		sendResponse({"type": "service-worker-summary-result", "data": error});
+	});
+	return true;
 });
